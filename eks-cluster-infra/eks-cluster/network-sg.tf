d6caab22122ab1301689623331177414748d5d79
paradination <jjy2k4life@yahoo.com>
2023-04-20 09:25:00 -0500

    control plane ec2

--- eks-cluster-infra/eks-cluster/network-sg.tf
+++ eks-cluster-infra/eks-cluster/network-sg.tf
@@ -61,3 +61,21 @@ resource "aws_route_table_association" "b" {
 output "subnetid1" {
   value = aws_subnet.main-subnet[*].id
 }
+
+
+#security group
+resource "aws_security_group" "sg" {
+  name        = "dynamic-sg"
+  description = "Ingress for Vault"
+  vpc_id = aws_vpc.main-vpc.id
+  dynamic "ingress" {
+    for_each = var.port-sg
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
